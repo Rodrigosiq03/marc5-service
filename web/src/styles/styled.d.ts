ec6d1d7782de813b65ec96de9cf9331fe911d9e1
Rafael Bidetti <rafael.bidetti@gmail.com>
2024-10-17 11:53:39 -0300

    feat: added theme control

---
+++ web/src/styles/styled.d.ts
@@ -0,0 +1,55 @@
+import 'styled-components';
+
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    title: string;
+
+    colors: {
+      background: string,
+      primary: string,
+      green_500: string,
+      green_300: string,
+
+      input: {
+        background: string,
+        placeholder: string,
+        border: string,
+        background_hover: string,
+        border_hover: string,
+      },
+      icons: {
+        color: string,
+        background: string,
+        background_hover: string,
+        border: string,
+      }
+    },
+  
+    fontsSizes: {
+      desktop: {
+        h1: string,
+        h2: string,
+        h3: string,
+        h4: string,
+        h5: string,
+        p: string,
+        p_small: string,
+        caption: string,
+      },
+      mobile: {
+        h1: string,
+        h2: string,
+        h3: string,
+        h4: string,
+        h5: string,
+        p: string,
+        p_small: string,
+        caption: string,
+      },
+      colors: {
+        black: string,
+        white: string,
+      }
+    }
+  }
+}
---
+++ web/src/styles/themes/dark.ts
@@ -0,0 +1,52 @@
+export default {
+  title: "dark",
+
+  colors: {
+    background: "#303030",
+    primary: "#FFFFFF",
+    green_500: "#049865",
+    green_300: "#13BB81",
+
+    input: {
+      background: "#4B4B4B",
+      placeholder: "rgba(75, 75, 75, 0.3)",
+      border: "rgba(0, 0, 0, 0.2)",
+      background_hover: "rgba(75, 75, 75, 0.7)",
+      border_hover: "#13BB81",
+    },
+
+    icons: {
+      color: "#FFFFFF",
+      background: "transparent",
+      background_hover: "#13BB81",
+      border: "#13BB81",
+    },
+  },
+
+  fontsSizes: {
+    desktop: {
+      h1: "4rem",
+      h2: "3.5rem",
+      h3: "2.5rem",
+      h4: "2rem",
+      h5: "1.5rem",
+      p: "0.875rem",
+      p_small: "0.625rem",
+      caption: "0.53125rem",
+    },
+    mobile: {
+      h1: "2.75rem",
+      h2: "2.25rem",
+      h3: "1.5rem",
+      h4: "1.125rem",
+      h5: "1rem",
+      p: "0.875rem",
+      p_small: "0.625rem",
+      caption: "0.53125rem",
+    },
+    colors: {
+      black: "#000000",
+      white: "#FFFFFF",
+    },
+  },
+};
