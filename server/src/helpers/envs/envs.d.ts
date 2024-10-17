04c3c9814a485ad5a655ceeb29d46cd879272c50
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-10-16 21:41:19 -0300

    feat: Add environment variables declaration file
    This commit adds a new file `envs.d.ts` to declare the environment variables used in the server code. This will provide type checking and autocompletion support for these variables.
    
    The `envs.d.ts` file includes the following environment variables:
    - AWS_ACCOUNT_ID
    - GITHUB_REF_NAME
    - STACK_NAME
    - AWS_REGION
    - STAGE
    - MONGO_URI
    
    These variables are declared in the `NodeJS.ProcessEnv` namespace.

---
+++ server/src/helpers/envs/envs.d.ts
@@ -0,0 +1,10 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    AWS_ACCOUNT_ID: string;
+    GITHUB_REF_NAME: string;
+    STACK_NAME: string;
+    AWS_REGION: string;
+    STAGE: string;
+    MONGO_URI: string;
+  }
+}
