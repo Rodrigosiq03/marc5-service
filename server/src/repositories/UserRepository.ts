2c205be3f20470da0e61337c24e345b6c0737488
LUIGI GUIMARAES TREVISAN <22.01102-0@maua.br>
2024-11-06 21:54:29 -0300

    UserRepository started with createUser

---
+++ server/src/repositories/UserRepository.ts
@@ -0,0 +1,14 @@
+import { connectDB } from "../database/connection";
+import User from "../domain/entities/User";
+
+export class UserRepository {
+
+    async createUser(user: User) {
+        var con = await connectDB();
+        console.log('Creating user:', user);
+        const db = con.connection.db;
+        const collection = db!.collection('users');
+        await collection.insertOne(user);
+        console.log(`User ${user.name} created`);
+    }
+}
