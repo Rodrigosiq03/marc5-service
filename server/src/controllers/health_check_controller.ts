ebbafb03d53aa6cd6dec4c891b7abcaaf601c1d9
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-11-13 20:55:44 -0300

    Update: add HealthCheckController to manage health check requests

---
+++ server/src/controllers/health_check_controller.ts
@@ -0,0 +1,29 @@
+import { NextFunction, Request, RequestHandler, Response } from "express";
+import { HealthCheckUsecase } from "../application/usecases/health_check_usecase";
+import { envs } from "../helpers/envs";
+import { HealthRepositoryMongo } from "../repositories/HealthRepository";
+
+class HealthCheckController {
+  constructor(
+    private readonly usecase: HealthCheckUsecase
+  ) {}
+
+
+  async handle (req: Request, res: Response, next?: NextFunction) {
+    try {
+      const health = await this.usecase.execute()
+      return res.status(200).json(health);
+    } catch (error: any) {
+      if (error instanceof Error) {
+        return res.status(500).send({ message: error.message });
+      }
+    }
+  }
+}
+
+
+const healthRepo = new HealthRepositoryMongo();
+const healthUsecase = new HealthCheckUsecase(healthRepo);
+const healthController = new HealthCheckController(healthUsecase);
+
+export const healthCheckHandler = healthController.handle.bind(healthController);
