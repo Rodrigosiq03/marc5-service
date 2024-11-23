65e0ee0429512a258d73cfd23c5d60701139cabe
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-11-23 16:22:41 -0300

    fix: reorganiza o servidor para suporte a ambientes serverless e adiciona rota de saúde

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,18 +3,26 @@ import cors from 'cors'
 import ServerlessHttp from 'serverless-http'
 import { envs } from './helpers/envs'
 import { healthRouter } from './routers/health_router'
+import { connectDB } from './database/connection'
+import { HealthDocument, healthModel } from './database/models/health'
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
-app.get('/health', (req, res) => {
-  res.status(200).json({ status: 'ok' })
-})
+app.use(healthRouter)
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000')
-})
 
-// module.exports.handler = ServerlessHttp(app)
 
+if (envs.STAGE !== 'test' && envs.STAGE !== 'dev') {
+  const handler = async (event: any, context: any) => {
+    return ServerlessHttp(app)
+  }
+
+  module.exports.handler = handler
+  
+} else {
+  app.listen(3000, async () => {
+    console.log('Server is running on port 3000')
+  })
+}
