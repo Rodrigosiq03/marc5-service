eb944bee192606a98f3a10f623361ba83cb13724
Rafael Bidetti <rafael.bidetti@gmail.com>
2024-11-24 14:59:03 -0300

    update: planos

--- web/src/components/PlanCard/index.tsx
+++ web/src/components/PlanCard/index.tsx
@@ -4,22 +4,26 @@ import {
   PlanTitle,
   PlanDescription,
   SubscribeButton,
+  FutureFeaturesList,
+  UpdatesSection,
+  UpdatesTitle,
 } from './styles';
 import PriceTag from '../PriceTag';
-import { Check } from '@phosphor-icons/react';
+import { Check, Clock } from '@phosphor-icons/react';
 import { ThemeContext } from 'styled-components';
 
-interface PlanCardProps {
+export interface PlanCardProps {
   title: string;
-  price: number;
+  price: number | 'Sob consulta';
   description: string[];
+  futureUpdates: string[];
 }
-
 
 const PlanCard: React.FC<PlanCardProps> = ({
   title,
   price,
-  description
+  description,
+  futureUpdates
 }) => {
   const theme = React.useContext(ThemeContext);
   return (
@@ -31,6 +35,17 @@ const PlanCard: React.FC<PlanCardProps> = ({
           <li key={index}><Check color={theme?.colors.icons.color}/>{item}</li>
         ))}
       </PlanDescription>
+      <UpdatesSection>
+        <UpdatesTitle>Em breve</UpdatesTitle>
+        <FutureFeaturesList>
+          {futureUpdates.map((item, index) => (
+            <li key={index}>
+              <Clock color={theme?.colors.icons.color} />
+              {item}
+            </li>
+          ))}
+        </FutureFeaturesList>
+      </UpdatesSection>
       <SubscribeButton>ASSINAR</SubscribeButton>
     </CardContainer>
   );
