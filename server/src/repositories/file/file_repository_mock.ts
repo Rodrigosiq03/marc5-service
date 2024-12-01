f63c7f90c8786e8a17e5f345e6f4e0fb587a06a8
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-12-01 18:58:54 -0300

    feat: adiciona implementação mock do repositório de arquivos para upload multipart

---
+++ server/src/repositories/file/file_repository_mock.ts
@@ -0,0 +1,23 @@
+import { IFileRepository } from "../../domain/repositories/file_repository_interface";
+
+export class FileRepositoryMock implements IFileRepository {
+  async createMultipartUpload(
+    courseId: string,
+    classId: string,
+  ): Promise<string | undefined> {
+    return 'mocked_upload_id';
+  }
+
+  async uploadPart(
+    courseId: string,
+    classId: string,
+    chunkIndex: number,
+    totalChunks: number,
+    fileBufferChunk: Buffer | undefined
+  ): Promise<{
+    uploadId: string | undefined,
+    ETag: string | undefined
+  } | undefined> {
+    return;
+  }
+}
