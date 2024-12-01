99eff122dc5617d448e8cc9c64adae0cfa735572
Rodrigo Siqueira <100361837+Rodrigosiq03@users.noreply.github.com>
2024-12-01 18:58:03 -0300

    feat: cria interface IFileRepository para suporte a upload multipart

---
+++ server/src/domain/repositories/file_repository_interface.ts
@@ -0,0 +1,16 @@
+export interface IFileRepository {
+  createMultipartUpload(
+    courseId: string,
+    classId: string,
+  ): Promise<string | undefined>;
+  uploadPart(
+    courseId: string,
+    classId: string,
+    chunkIndex: number,
+    totalChunks: number,
+    fileBufferChunk: Buffer | undefined
+  ): Promise<{
+    uploadId: string | undefined,
+    ETag: string | undefined
+  } | undefined>
+}
