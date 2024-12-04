cf53a5889b897e1f09a1c840a6f260992b16522f
Luigi Trevisan <checkzin123@gmail.com>
2024-12-03 23:54:46 -0300

    fixed Company and Creator constructors to include xp and pictureUrl parameters 🙈

--- server/src/domain/entities/Company.ts
+++ server/src/domain/entities/Company.ts
@@ -3,8 +3,8 @@ import Creator from "./Creator";
 export default class Company extends Creator {
     collaborators: string[]; // [userId]
 
-    constructor(userId: string, name: string, email: string, password: string, courses: string[], maintainedCourses: string[], collaborators: string[], createdBy?: string) {
-        super(userId, name, email, password, courses, maintainedCourses, createdBy);
+    constructor(userId: string, name: string, email: string, password: string, courses: string[], xp: number, maintainedCourses: string[], collaborators: string[], createdBy?: string, pictureUrl?: string) {
+        super(userId, name, email, password, courses, xp, maintainedCourses, createdBy, pictureUrl);
         this.collaborators = collaborators;
     }
 }
--- server/src/domain/entities/Creator.ts
+++ server/src/domain/entities/Creator.ts
@@ -3,8 +3,8 @@ import User from "./User";
 export default class Creator extends User {
     maintainedCourses: string[]; // [courseId]
 
-    constructor(userId: string, name: string, email: string, password: string, courses: string[], maintainedCourses: string[], createdBy?: string) {
-        super(name, email, password, courses, userId, createdBy);
+    constructor(userId: string, name: string, email: string, password: string, courses: string[], xp: number, maintainedCourses: string[], createdBy?: string, pictureUrl?: string) {
+        super(name, email, password, courses, xp, userId, createdBy, pictureUrl);
         this.maintainedCourses = maintainedCourses;
     }
 }
