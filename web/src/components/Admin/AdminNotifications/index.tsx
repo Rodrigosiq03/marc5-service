4eb391fd8d4ad213b4689966892cfd18c9f830a8
Leonardo Guilmo Chin <22.00512-9@maua.br>
2024-12-04 19:49:52 -0300

    feat: add "under construction" screen for admin notifications screen

---
+++ web/src/components/Admin/AdminNotifications/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container, Title, Image } from './styles';
+
+const AdminNotifications: React.FC = () => {
+    return (
+        <Container>
+            <Title>Em construção...</Title>
+            <Image src="/under_construction.png" alt="" />
+        </Container>
+    );
+};
+
+export default AdminNotifications;
